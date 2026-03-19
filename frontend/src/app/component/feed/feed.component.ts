8fa326527dac5d06867a00fa397de6ef89b45fd4
squasheland <janoka82@gmail.com>
2026-03-20 00:20:08 +0100

    basic tweet design

--- frontend/src/app/component/feed/feed.component.ts
+++ frontend/src/app/component/feed/feed.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { NewTweetComponent } from "./new_tweet/new_tweet.component";
+import { TweetComponent } from './tweet/tweet.component';
 
 @Component({
   selector: 'app-feed',
-  imports: [NewTweetComponent],
+  imports: [NewTweetComponent, TweetComponent],
   templateUrl: './feed.component.html',
   styleUrl: './feed.component.scss',
 })
---
+++ frontend/src/app/component/feed/tweet/tweet.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { NgOptimizedImage } from '@angular/common';
+import { MatIcon } from '@angular/material/icon';
+import { MatIconButton } from '@angular/material/button';
+
+@Component({
+  selector: 'app-tweet',
+  imports: [NgOptimizedImage, MatIcon, MatIconButton],
+  templateUrl: './tweet.component.html',
+  styleUrl: './tweet.component.scss',
+})
+export class TweetComponent {}
